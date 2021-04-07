6a8e283cc1ab43c01f7cfd17d15348eb9f290ac6
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2021-04-07 11:14:09 +0100

    chore: MP-135 Add Sonar support

---
+++ scripts/sonar.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+# ----------------------------------------
+# Ensure we're in the project root by
+# first moving to this directory and then
+# one out.
+# ----------------------------------------
+cd $(dirname $0)
+cd ..
+
+SONAR_LOGIN=""
+SCANNER_PATH="/usr/local/sonar-scanner/bin/sonar-scanner"
+
+if command -v "$SCANNER_PATH" &> /dev/null
+then
+  for i in "$@"
+  do
+  case $i in
+      --login=*) SONAR_LOGIN="${i#*=}"
+      shift # past argument
+      ;;
+      --scanner-path=*) SCANNER_PATH="${i#*=}"
+      shift # past argument
+      ;;
+  esac
+  done
+
+  export COVERAGE=true
+
+  yarn run each:test
+  yarn run each:lint:js -- -- --format json --output-file "./coverage/eslint.json"
+
+  eval "$SCANNER_PATH -Dsonar.login=$SONAR_LOGIN -Dproject.settings=./sonar-scanner.properties"
+
+  yarn run each:cleanup
+fi
+
