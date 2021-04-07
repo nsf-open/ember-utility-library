912b0ee36c80cd6cbc1600444b229cca67f35848
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2021-04-07 15:29:47 +0100

    chore: MP-135 SONAR_LOGIN accepted as env param

--- scripts/sonar.sh
+++ scripts/sonar.sh
@@ -9,7 +9,6 @@ set -e
 cd $(dirname $0)
 cd ..
 
-SONAR_LOGIN=""
 SCANNER_PATH="/usr/local/sonar-scanner/bin/sonar-scanner"
 
 if command -v "$SCANNER_PATH" &> /dev/null
@@ -26,13 +25,18 @@ then
   esac
   done
 
-  export COVERAGE=true
+  if [ -z "$SONAR_LOGIN" ]
+  then
+    echo "A Sonar auth token is required"
+  else
+    export COVERAGE=true
 
-  yarn run each:test
-  yarn run each:lint:js -- -- --format json --output-file "./coverage/eslint.json"
+    yarn run each:test
+    yarn run each:lint:js -- -- --format json --output-file "./coverage/eslint.json"
 
-  eval "$SCANNER_PATH -Dsonar.login=$SONAR_LOGIN -Dproject.settings=./sonar-scanner.properties"
+    eval "$SCANNER_PATH -Dsonar.login=$SONAR_LOGIN -Dproject.settings=./sonar-scanner.properties"
 
-  yarn run each:cleanup
+    yarn run each:cleanup
+  fi
 fi
 
