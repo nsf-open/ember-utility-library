2d5db6ec4926f38500b0684cff308d82c6bfad81
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2021-04-07 15:20:49 +0100

    test: MP-135 Decorators tests

--- packages/@nsf/currency-utils/tests/unit/formatters-test.ts
+++ packages/@nsf/currency-utils/tests/unit/formatters-test.ts
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import { toUSD, fromUSD } from '@nsf/currency-utils';
+import { toUSDFormat, fromUSDFormat } from '@nsf/currency-utils';
 
 
 module('Unit | Utility | formatters', function(hooks) {
@@ -8,36 +8,36 @@ module('Unit | Utility | formatters', function(hooks) {
 
 
 	test('toUSD()', function(assert) {
-		assert.strictEqual(toUSD(null), null);
-		assert.strictEqual(toUSD('ab'), 'ab');
-		assert.strictEqual(toUSD(Number.POSITIVE_INFINITY), Number.POSITIVE_INFINITY);
+		assert.strictEqual(toUSDFormat(null), null);
+		assert.strictEqual(toUSDFormat('ab'), 'ab');
+		assert.strictEqual(toUSDFormat(Number.POSITIVE_INFINITY), Number.POSITIVE_INFINITY);
 
-		assert.strictEqual(toUSD(123), '$123');
-		assert.strictEqual(toUSD(123, true), '$123.00');
-		assert.strictEqual(toUSD(123, { cents: false }), '$123');
-		assert.strictEqual(toUSD(123, { cents: true }), '$123.00');
-		assert.strictEqual(toUSD(123, { currencySymbol: true }), '$123');
-		assert.strictEqual(toUSD(123, { currencySymbol: false }), '123');
-		assert.strictEqual(toUSD(123, { currencySymbol: false, cents: true }), '123.00');
+		assert.strictEqual(toUSDFormat(123), '$123');
+		assert.strictEqual(toUSDFormat(123, true), '$123.00');
+		assert.strictEqual(toUSDFormat(123, { cents: false }), '$123');
+		assert.strictEqual(toUSDFormat(123, { cents: true }), '$123.00');
+		assert.strictEqual(toUSDFormat(123, { currencySymbol: true }), '$123');
+		assert.strictEqual(toUSDFormat(123, { currencySymbol: false }), '123');
+		assert.strictEqual(toUSDFormat(123, { currencySymbol: false, cents: true }), '123.00');
 
-		assert.strictEqual(toUSD(-123), '-$123');
-		assert.strictEqual(toUSD(-123, true), '-$123.00');
-		assert.strictEqual(toUSD(-123, { accountingFormat: true }), '($123)');
-		assert.strictEqual(toUSD(-123, { accountingFormat: true, currencySymbol: false }), '(123)');
-		assert.strictEqual(toUSD(-123, { accountingFormat: true, currencySymbol: false, cents: true }), '(123.00)');
+		assert.strictEqual(toUSDFormat(-123), '-$123');
+		assert.strictEqual(toUSDFormat(-123, true), '-$123.00');
+		assert.strictEqual(toUSDFormat(-123, { accountingFormat: true }), '($123)');
+		assert.strictEqual(toUSDFormat(-123, { accountingFormat: true, currencySymbol: false }), '(123)');
+		assert.strictEqual(toUSDFormat(-123, { accountingFormat: true, currencySymbol: false, cents: true }), '(123.00)');
 	});
 
 
 	test('fromUSD()', function(assert) {
-		assert.ok(isNaN(fromUSD(null)));
-		assert.strictEqual(fromUSD(123), 123);
-		assert.strictEqual(fromUSD(Number.POSITIVE_INFINITY), Number.POSITIVE_INFINITY);
+		assert.ok(isNaN(fromUSDFormat(null)));
+		assert.strictEqual(fromUSDFormat(123), 123);
+		assert.strictEqual(fromUSDFormat(Number.POSITIVE_INFINITY), Number.POSITIVE_INFINITY);
 
-		assert.strictEqual(fromUSD('$123'), 123);
-		assert.strictEqual(fromUSD('$123.00'), 123);
-		assert.strictEqual(fromUSD('$123.55'), 123.55);
-		assert.strictEqual(fromUSD('  $123.55'), 123.55);
-		assert.strictEqual(fromUSD('-$123.55'), -123.55);
-		assert.strictEqual(fromUSD('($123.55)'), -123.55);
+		assert.strictEqual(fromUSDFormat('$123'), 123);
+		assert.strictEqual(fromUSDFormat('$123.00'), 123);
+		assert.strictEqual(fromUSDFormat('$123.55'), 123.55);
+		assert.strictEqual(fromUSDFormat('  $123.55'), 123.55);
+		assert.strictEqual(fromUSDFormat('-$123.55'), -123.55);
+		assert.strictEqual(fromUSDFormat('($123.55)'), -123.55);
 	});
 });
