f3341059f51981b66111492674854260ba8a6ce8
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2021-04-11 12:26:09 +0100

    chore: is-primitive and guid-for tests

---
+++ packages/@nsf/general-utils/tests/integration/helpers-test.ts
@@ -0,0 +1,46 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { guidFor } from '@ember/object/internals';
+
+
+module('Integration | Helpers', function(hooks) {
+	setupRenderingTest(hooks);
+
+
+	function getText(container: Element) {
+		return container.querySelector('[data-test]')?.textContent;
+	}
+
+
+	test('{{is-primitive}}', async function(assert) {
+		this.set('testValue', 0);
+		await render(hbs`<span data-test>{{if (is-primitive this.testValue) "TRUE" "FALSE"}}</span>`);
+		assert.strictEqual(getText(this.element), 'TRUE');
+
+		this.set('testValue', "0");
+		assert.strictEqual(getText(this.element), 'TRUE');
+
+		this.set('testValue', true);
+		assert.strictEqual(getText(this.element), 'TRUE');
+
+		this.set('testValue', {});
+		assert.strictEqual(getText(this.element), 'FALSE');
+
+		this.set('testValue', []);
+		assert.strictEqual(getText(this.element), 'FALSE');
+	});
+
+
+	test('{{guid-for}}', async function(assert) {
+		this.set('testValue', {});
+		this.set('testSuffix', undefined);
+
+		await render(hbs`<span data-test>{{guid-for this.testValue this.testSuffix}}</span>`);
+		assert.strictEqual(getText(this.element), guidFor(this.get('testValue')));
+
+		this.set('testSuffix', 'foobar');
+		assert.strictEqual(getText(this.element), `${guidFor(this.get('testValue'))}-foobar`);
+	});
+});
