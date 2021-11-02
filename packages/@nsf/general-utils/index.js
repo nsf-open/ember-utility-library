0863df3e61a6e5859ba38f6e13d2854dc4f14d88
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2021-11-02 12:48:26 -0400

    fix: remove invalid MomentJS import that would cause build to fail if the library wasn't available

--- packages/@nsf/general-utils/index.js
+++ packages/@nsf/general-utils/index.js
@@ -2,11 +2,4 @@
 
 module.exports = {
 	name: require('./package').name,
-
-	included() {
-		this._super.included.apply(this, arguments);
-		this.import('node_modules/moment/min/moment.min.js', {
-			using: [{ transformation: 'amd', as: 'moment' }],
-		});
-	}
 };
