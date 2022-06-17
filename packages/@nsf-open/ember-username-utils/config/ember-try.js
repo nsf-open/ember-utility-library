3aae490656b44a29205e24e5b9df84c346bff217
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2022-06-17 10:12:47 -0400

    test: setup ember-try scenarios

--- packages/@nsf-open/ember-username-utils/config/ember-try.js
+++ packages/@nsf-open/ember-username-utils/config/ember-try.js
@@ -1,38 +1,48 @@
 'use strict';
-
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
-module.exports = function() {
-	return Promise.all([
-		getChannelURL('release')
-	]).then((urls) => {
-		return {
-			useYarn: true,
+module.exports = async function() {
+	return {
+		useYarn: true,
+		command: 'npm run test:ember',
 
-			scenarios: [
-				{
-					name: 'ember-lts-3.4',
-					npm: {
-						devDependencies: {
-							'ember-source': '~3.4.0'
-						}
+		scenarios: [
+			{
+				name: 'ember-lts-3.16',
+				npm: {
+					devDependencies: {
+						'ember-source': '~3.16'
 					}
-				},
-				{
-					name: 'ember-release',
-					npm: {
-						devDependencies: {
-							'ember-source': urls[0]
-						}
+				}
+			},
+			{
+				name: 'ember-lts-3.28',
+				npm: {
+					devDependencies: {
+						'ember-source': '~3.28'
 					}
+				}
+			},
+			{
+				name: 'ember-release',
+				env: {
+					COVERAGE: true,
 				},
-				{
-					name: 'ember-default',
-					npm: {
-						devDependencies: {}
+				npm: {
+					devDependencies: {
+						'ember-source': await getChannelURL('release'),
 					}
-				},
-			]
-		};
-	});
+				}
+			},
+			{
+				name: 'ember-default',
+				npm: {
+					devDependencies: {}
+				}
+			},
+			embroiderSafe(),
+			embroiderOptimized(),
+		]
+	};
 };
